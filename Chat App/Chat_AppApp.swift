1e92434aa91d03fb06f49aaabed1ea79392063e6
Mrsazza <shahriarhossain147@gmail.com>
2022-04-23 11:59:26 +0600

    Initial Commit

---
+++ Chat App/Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppApp.swift
+//  Chat App
+//
+//  Created by Sazza on 23/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
